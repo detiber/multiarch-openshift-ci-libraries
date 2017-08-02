37f89b131663d388a72f3485e4a152179145449c
Jason DeTiberus <jdetiber@redhat.com>
2017-08-02 17:03:31 -0400

    Add nodes library

---
+++ vars/nodes.groovy
@@ -0,0 +1,19 @@
+class nodes implements Serializable {
+  private List<String> nodes
+
+  nodes() {
+    nodes = new List<String>()
+    Jenkins.instance.nodes.each { node ->
+      nodes.append(node.name)
+    }
+    this.nodes = nodes
+  }
+
+  List<String> getNodes() {
+    return this.nodes
+  }
+
+  boolean nodeExists(String name) {
+    return this.nodes.contains(name)
+  }
+}
