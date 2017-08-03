b2753548ad03fd9c0a5315255be518a36de10239
Jason DeTiberus <jdetiber@redhat.com>
2017-08-02 22:03:11 -0400

    test

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,9 +1,18 @@
-def hello_world() {
-  echo "Hello World!"
-}
-
 //class hello implements Serializable {
 //  def world() {
 //    echo "Hello World!"
 //  }
 //}
+
+class hello implements Serializable {
+    private String name
+    def setName(value) {
+        name = value
+    }
+    def getName() {
+        name
+    }
+    def greeting(message) {
+        echo "Hello, ${name}!"
+    }
+}
